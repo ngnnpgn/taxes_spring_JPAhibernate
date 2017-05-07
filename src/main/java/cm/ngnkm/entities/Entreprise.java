37f6062846f32cb75a0462ca5b9bda095ead7636
ngnnpgn <fngankam@gmail.com>
2017-05-07 15:28:51 +0200

    Creation controleur - Vue(html, bootstrap, thymeleaf) 1

--- src/main/java/cm/ngnkm/entities/Entreprise.java
+++ src/main/java/cm/ngnkm/entities/Entreprise.java
@@ -18,38 +18,48 @@ public class Entreprise implements Serializable{
 	private String nom;
 	private String email;
 	private String raisonSociale;
+	
 	@OneToMany(mappedBy="entreprise", fetch = FetchType.LAZY)
 	private Collection<Taxe> taxes;
 	public Long getCode() {
 		return code;
 	}
+	
 	public void setCode(Long code) {
 		this.code = code;
 	}
+	
 	public String getNom() {
 		return nom;
 	}
+	
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getRaisonSociale() {
 		return raisonSociale;
 	}
+	
 	public void setRaisonSociale(String raisonSociale) {
 		this.raisonSociale = raisonSociale;
 	}
 	public Collection<Taxe> getTaxes() {
 		return taxes;
 	}
+	
 	public void setTaxes(Collection<Taxe> taxes) {
 		this.taxes = taxes;
 	}
+	
 	public Entreprise() {
 		super();
 		// TODO Auto-generated constructor stub
--- src/main/java/cm/ngnkm/web/TaxeControler.java
+++ src/main/java/cm/ngnkm/web/TaxeControler.java
@@ -1,5 +1,29 @@
 package cm.ngnkm.web;
 
-public class TaxeControler {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import cm.ngnkm.dao.EntrepriseRepository;
+import cm.ngnkm.entities.Entreprise;
+
+@Controller
+public class TaxeControler {
+	
+	@Autowired
+	private EntrepriseRepository entrepriseRepository;
+	
+	/*retourne une vue de nom entreprise - affiche les entreprises */
+	@RequestMapping(value="/entreprises", method = RequestMethod.GET)
+	public String index(Model model) {
+		
+		List<Entreprise> entreprises = entrepriseRepository.findAll();
+		model.addAttribute("listeEntreprises", entreprises);
+		return "entreprises";
+		
+	}
 }
