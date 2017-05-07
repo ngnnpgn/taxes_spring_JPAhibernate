d13e78f54e940cb061f348e2267b5e94a9f39862
ngnnpgn <fngankam@gmail.com>
2017-05-07 19:42:52 +0200

    Rechercher, Table, Form - Vue(html, bootstrap, thymeleaf) 3

--- src/main/java/cm/ngnkm/dao/EntrepriseRepository.java
+++ src/main/java/cm/ngnkm/dao/EntrepriseRepository.java
@@ -1,9 +1,16 @@
 package cm.ngnkm.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import cm.ngnkm.entities.Entreprise;
 
 public interface EntrepriseRepository extends JpaRepository<Entreprise, Long>{
+	@Query("select e from Entreprise e where e.nom like :x" )
+	public Page<Entreprise> chercher(@Param("x")String e, Pageable pageable);
+	
 
 }
--- src/main/java/cm/ngnkm/web/TaxeControler.java
+++ src/main/java/cm/ngnkm/web/TaxeControler.java
@@ -3,10 +3,13 @@ package cm.ngnkm.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import cm.ngnkm.dao.EntrepriseRepository;
 import cm.ngnkm.entities.Entreprise;
@@ -19,11 +22,18 @@ public class TaxeControler {
 	
 	/*retourne une vue de nom entreprise - affiche les entreprises */
 	@RequestMapping(value="/entreprises", method = RequestMethod.GET)
-	public String index(Model model) {
+	public String index(Model model,
+			@RequestParam(name="motCle", defaultValue="")String m,
+			@RequestParam(name="page", defaultValue="0")int p, 
+			@RequestParam(name="size", defaultValue="4")int s) {
 		
-		List<Entreprise> entreprises = entrepriseRepository.findAll();
-		model.addAttribute("listeEntreprises", entreprises);
-		return "entreprises";
-		
+		Page<Entreprise> pageEntreprises = entrepriseRepository.chercher("%"+m+ "%", new PageRequest(p, s));
+		model.addAttribute("listeEntreprises", pageEntreprises.getContent());
+		int[] pages = new int[pageEntreprises.getTotalPages()];
+		model.addAttribute("pages",pages);
+		model.addAttribute("pageCourante", p);
+		model.addAttribute("motCle", m);
+		return "entreprises";	
 	}
+	
 }
